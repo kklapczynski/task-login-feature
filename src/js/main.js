8128ae76cd0333915f56d381f18f750c3fd1b2f8
Krzysztof Klapczyński <k.klapczynski@gmail.com>
2019-04-03 13:57:03 +0200

    DRYs js code. Resolves minor responsivness issues.

--- src/js/main.js
+++ src/js/main.js
@@ -1,162 +1,140 @@
-const form = document.getElementsByClassName('login-container')[0];
-const headerText = document.getElementById('header-text');
-const resetPassInfo = document.getElementById('reset-pass-info');
-const emailLabel = document.getElementById('email-label');
-const emailInput = document.getElementById('email');
-const emailErrorMessage = document.getElementById('email-error-message');
-const passwordContainer = document.getElementById('password-container');
-const passwordLabel = document.getElementById('password-label');
-const passwordInput = document.getElementById('password');
-const passwordErrorMessage = document.getElementById('password-error-message');
-const rememberMeContainer = document.getElementById('remember-me-container');
-const loginButton = document.getElementById('login-button');
-const resetPassButton = document.getElementById('reset-pass-button');
-const forgottenPassword = document.getElementById('login-footer-text');
-const loginFooter = document.getElementsByClassName('login-footer')[0];
-const cancelButton = document.getElementById('cancel-reset-button');
-const loginModeOnlyElements = document.getElementsByClassName('login-mode-only')[0];
+const form = document.getElementsByClassName('login-container')[0]
+const emailLabel = document.getElementById('email-label')
+const emailInput = document.getElementById('email')
+const emailErrorMessage = document.getElementById('email-error-message')
+const passwordLabel = document.getElementById('password-label')
+const passwordInput = document.getElementById('password')
+const passwordErrorMessage = document.getElementById('password-error-message')
+const loginButton = document.getElementById('login-button')
+const resetPassButton = document.getElementById('reset-pass-button')
+const forgottenPassword = document.getElementById('login-footer-text')
+const loginFooter = document.getElementsByClassName('login-footer')[0]
+const cancelButton = document.getElementById('cancel-reset-button')
 
+const loginModeElements = document.getElementsByClassName('login-mode')
+const resetPasswordElements = document.getElementsByClassName('reset-password-mode')
 
-
-
-// DONE: check validity for error message display on losing focus
+// check validity for error message display on losing focus
 form.addEventListener('change', (event) => {
-    if(event.target.id === 'email' && !emailInput.validity.valid) {
-        showEmailError();
-    }
-    if(event.target.id === 'password' && !passwordInput.validity.valid) {
-        showPasswordError();
-    }
-    return;
-});
-
-// DONE: remove error message on input when is valid
-form.addEventListener('input', (event) => {
-    if(event.target.id === 'email' && emailInput.validity.valid) {
-        hideEmailError();
-    }
-    if(event.target.id === 'password' && passwordInput.validity.valid) {
-        hidePasswordError();
-    }
-
-    // toggle disable attribute of login button on both inputs validity
-    if((event.target.id === 'email' || event.target.id === 'password')) {
-        if(loginMode) {
-            (emailInput.checkValidity() && passwordInput.checkValidity())
-            ? loginButton.removeAttribute('disabled')
-            : loginButton.setAttribute('disabled', 'disabled');
-        } else if(!loginMode) {
-            (emailInput.checkValidity())
-            ? resetPassButton.removeAttribute('disabled')
-            : resetPassButton.setAttribute('disabled', 'disabled');
-        }
-    }
-    return;
-});
-
-form.addEventListener("submit", function (event) {
-    // redundand as submit button is disabled when inputs not valid
-    // if(!email.validity.valid) {
-    //     emailErrorMessage.innerHTML = 'Invalid email address';
-    //     emailErrorMessage.className = 'error error-message';
-    //     emailLabel.className = 'error';
-    // }
-    event.preventDefault();
-    sendData();
-});
-
-forgottenPassword.addEventListener('click', () => {
-    switchToResetPassword();
-    clearInput(emailInput);
-    hideEmailError();
-    return;
-});
-
-cancelButton.addEventListener('click', () => {
-    switchToLogin();
-    clearInput(emailInput);
-    hideEmailError();
-    clearInput(passwordInput);
-    hidePasswordError();
-    return;
+  if (event.target.id === 'email' && !emailInput.validity.valid) {
+    showEmailError()
+  }
+  if (event.target.id === 'password' && !passwordInput.validity.valid) {
+    showPasswordError()
+  }
 })
 
-const clearInput = function(inputEl) {
-    inputEl.value = '';
-    return;
+// remove error message on input when is valid
+form.addEventListener('input', (event) => {
+  if (event.target.id === 'email' && emailInput.validity.valid) {
+    hideEmailError()
+  }
+  if (event.target.id === 'password' && passwordInput.validity.valid) {
+    hidePasswordError()
+  }
+
+  // toggle disable attribute of login button on both inputs validity
+  if ((event.target.id === 'email' || event.target.id === 'password')) {
+    if (loginMode) {
+      (emailInput.checkValidity() && passwordInput.checkValidity())
+        ? loginButton.removeAttribute('disabled')
+        : loginButton.setAttribute('disabled', 'disabled')
+    } else if (!loginMode) {
+      (emailInput.checkValidity())
+        ? resetPassButton.removeAttribute('disabled')
+        : resetPassButton.setAttribute('disabled', 'disabled')
+    }
+  }
+})
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+  sendData()
+})
+
+forgottenPassword.addEventListener('click', () => {
+  switchToResetPassword()
+  clearInput(emailInput)
+  hideEmailError()
+})
+
+cancelButton.addEventListener('click', () => {
+  switchToLogin()
+  clearInput(emailInput)
+  hideEmailError()
+  clearInput(passwordInput)
+  hidePasswordError()
+})
+
+const clearInput = function (inputEl) {
+  inputEl.value = ''
 }
 
-const showEmailError = function() {
-    emailErrorMessage.innerHTML = 'Invalid email address';
-    email.classList.add('error-border');
-    emailLabel.classList.add('error-color');
-    return;
+const showEmailError = function () {
+  emailErrorMessage.innerHTML = 'Invalid email address'
+  emailInput.classList.add('error-border')
+  emailLabel.classList.add('error-color')
 }
 
-const hideEmailError = function() {
-    emailErrorMessage.innerHTML = '';
-    email.classList.remove('error-border');
-    emailLabel.classList.remove('error-color');
-    return;
+const hideEmailError = function () {
+  emailErrorMessage.innerHTML = ''
+  emailInput.classList.remove('error-border')
+  emailLabel.classList.remove('error-color')
 }
 
-const showPasswordError = function() {
-    passwordErrorMessage.innerHTML = 'Invalid password';
-    password.classList.add('error-border');
-    passwordLabel.classList.add('error-color');
-    return;
+const showPasswordError = function () {
+  passwordErrorMessage.innerHTML = 'Invalid password'
+  passwordInput.classList.add('error-border')
+  passwordLabel.classList.add('error-color')
 }
 
-const hidePasswordError = function() {
-    passwordErrorMessage.innerHTML = '';
-    password.classList.remove('error-border');
-    passwordLabel.classList.remove('error-color');
-    return;
+const hidePasswordError = function () {
+  passwordErrorMessage.innerHTML = ''
+  passwordInput.classList.remove('error-border')
+  passwordLabel.classList.remove('error-color')
 }
 
-let loginMode = true;
-// array of el. to hide
-// array of el. to show
-const switchToResetPassword = function() {
-    loginMode = !loginMode;
-    // hideEl([elToHide])
-    // showEl([elToShow])
-    headerText.classList.remove('hide');
-    resetPassInfo.classList.remove('hide');
-    // loginModeOnlyElements.classList.add('hide');
-    passwordContainer.classList.add('hide');
-    rememberMeContainer.classList.add('hide');
-    loginButton.classList.add('hide');
-    resetPassButton.classList.remove('hide');
-    cancelButton.classList.remove('hide');
-    loginFooter.classList.add('space-between');
-    forgottenPassword.classList.add('hide');
-    return;
-};
+// flag for submit button
+let loginMode = true
 
-const switchToLogin = function() {
-    loginMode = !loginMode;
-    // hideEl([elToHide])
-    headerText.classList.add('hide');
-    resetPassInfo.classList.add('hide');
-    passwordContainer.classList.remove('hide');
-    rememberMeContainer.classList.remove('hide');
-    loginButton.classList.remove('hide');
-    resetPassButton.classList.add('hide');
-    cancelButton.classList.add('hide');
-    loginFooter.classList.remove('space-between');
-    forgottenPassword.classList.remove('hide');
-    return;
-};
+const switchToResetPassword = function () {
+  // switch flag
+  loginMode = !loginMode
 
-const sendData = function() {
-    // AJAX call using XMLHttpRequest
-    let alertText;
-    loginMode
-        ? alertText='Pretending your login data was sent for authentication.'
-        : alertText='Pretending reset password request was send to server.';
-    alert(alertText);
-    return;
+  // show reset-password-mode
+  Array.from(resetPasswordElements).forEach(el => el.classList.remove('hide'))
+
+  // hide login-mode elements
+  Array.from(loginModeElements).forEach(el => el.classList.add('hide'))
+
+  // give space-between content of footer to separate cancel and reset password buttons
+  loginFooter.classList.add('space-between')
 }
 
+const switchToLogin = function () {
+  // switch flag
+  loginMode = !loginMode
 
+  // show login-mode elements
+  Array.from(loginModeElements).forEach(el => el.classList.remove('hide'))
+
+  // hide reset-password-mode elements
+  Array.from(resetPasswordElements).forEach(el => el.classList.add('hide'))
+
+  // go back to default: content on the right
+  loginFooter.classList.remove('space-between')
+}
+
+const sendData = function () {
+  // here normally goes AJAX call using XMLHttpRequest to server
+  // in login case for its authentication
+
+  // just to show that something happens after submitting the form
+  let alertText
+
+  loginMode
+    ? alertText = 'Pretending your login data was sent for authentication.'
+    : alertText = 'Pretending reset password request was send to server.'
+
+  alert(alertText)
+}
